0f6214b430c0111497e23368fc57779f27d7456c
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-04-28 13:44:59 +0200

    added bash aliases

--- __init__.sh
+++ __init__.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 
+source ~/.bashrc.d/bash.sh
 source ~/.bashrc.d/maven.sh
---
+++ bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o pipefail
+
+# create temp dir and cd
+alias cdtmp='dir="$(tempfile).d" && mkdir -p $dir && cd $dir'
+
+# create dir and cd
+function cdmkdir {
+	local dir=$1 || echo "ERROR missing argument"
+	mkdir -p $dir && cd $dir
+}

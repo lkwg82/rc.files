3d15d6c94c3f38bfe50c09b5ad3f04f40ec19b5f
Lars Gohlke <lars.gohlke@smarthelios.de>
2018-07-27 14:03:47 +0200

    added default ll alias for mac

--- bash.sh
+++ bash.sh
@@ -5,6 +5,9 @@ set -o pipefail
 # create temp dir and cd
 alias cdtmp='dir="$(tempfile).d" && mkdir -p $dir && cd $dir'
 
+alias ll='ls -al'
+
+
 # create dir and cd
 function cdmkdir {
 	local dir=$1 || echo "ERROR missing argument"

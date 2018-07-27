6eccae5a7ac5547ef5e7b386683b80e373709f98
Lars Gohlke <lars.gohlke@smarthelios.de>
2018-07-27 17:45:02 +0200

    adds .vimrc

---
+++ .vimrc
@@ -0,0 +1,15 @@
+colo desert
+syntax on
+
+" http://vimconfig.com/
+
+set showmatch	" Highlight matching brace
+set visualbell	" Use visual bell (no beeping)
+ 
+set hlsearch	" Highlight all search results
+set smartcase	" Enable smart-case search
+set ignorecase	" Always case-insensitive
+set autoindent	" Auto-indent new lines
+
+set showmatch
+set ruler
--- __init__.sh
+++ __init__.sh
@@ -5,7 +5,14 @@ source ~/.bashrc.d/bash.sh
 source ~/.bashrc.d/gradle.sh
 source ~/.bashrc.d/maven.sh
 
-if [ ! -f "$HOME/.inputrc" ]; then
-  echo "first time linked .inputrc"
-  ln -vfs ~/.bashrc.d/.inputrc ~
-fi
+function linkFile {
+  local file=$1
+     
+  if [ ! -f "$HOME/$file" ]; then
+    echo "first time linked '$file'"
+    ln -vfs ~/.bashrc.d/$file ~
+  fi
+}
+
+linkFile .inputrc
+linkFile .vimrc

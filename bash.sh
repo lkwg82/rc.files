b7a0b6c70e6b33215b20e830867899b8d914c12c
Lars Gohlke <lars.gohlke@smarthelios.de>
2018-07-27 18:39:51 +0200

    adds colors to ls

--- bash.sh
+++ bash.sh
@@ -7,6 +7,9 @@ alias cdtmp='dir="$(tempfile).d" && mkdir -p $dir && cd $dir'
 
 alias ll='ls -al'
 
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='ls -GFh'
 
 # create dir and cd
 function cdmkdir {

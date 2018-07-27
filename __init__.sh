0c1b7e06921e75b045ea067e4595fcc464597093
Lars Gohlke <lars.gohlke@smarthelios.de>
2018-07-27 14:20:38 +0200

    adds keymapping for bash history search

--- __init__.sh
+++ __init__.sh
@@ -4,3 +4,8 @@
 source ~/.bashrc.d/bash.sh
 source ~/.bashrc.d/gradle.sh
 source ~/.bashrc.d/maven.sh
+
+if [ ! -f "$HOME/.inputrc" ]; then
+  echo "first time linked .inputrc"
+  ln -vfs ~/.bashrc.d/.inputrc ~
+fi

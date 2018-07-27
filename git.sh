da1a25f23862c23f477b180ad8d95efd2d810d7a
Lars Gohlke <lars.gohlke@smarthelios.de>
2018-07-27 18:40:21 +0200

    changes git prompt

--- git.sh
+++ git.sh
@@ -7,3 +7,11 @@ set -o pipefail
 if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
   . `brew --prefix`/etc/bash_completion.d/git-completion.bash
 fi
+
+if [ -f /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+  . /usr/local/etc/bash_completion.d/git-prompt.sh
+  export GIT_PS1_SHOWCOLORHINTS=true
+  export GIT_PS1_SHOWDIRTYSTATE=true
+  export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
+  export PS1='[\u@mbp \w$(__git_ps1)]\$ '
+fi

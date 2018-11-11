e415a4c37113f7716487b3dfef7a1585906f1c40
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-11 13:52:19 +0100

    adds linuxbrew to harmonize app installation

--- init_tools.sh
+++ init_tools.sh
@@ -3,14 +3,28 @@
 set -o pipefail
 
 function initMacosX {
-    for p in wget htop curl; do
-        which ${p} > /dev/null || brew install ${p};
-    done
     ssh-add -K
 }
 
 if [ ${platform} = "darwin" ]; then
+   which brew > /dev/null
+   if [ $? -eq 1 ]; then
+       echo "install brew -> https://brew.sh"
+   fi
    initMacosX
 fi
 
+if [ ${platform} = "linux" ]; then
+    export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+    which brew > /dev/null
+    if [ $? -eq 1 ]; then
+       echo "install brew -> https://linuxbrew.sh/"
+   fi
+fi
 
+which brew > /dev/null
+if [ $? -eq 0 ]; then
+    for p in wget htop curl; do
+        which ${p} > /dev/null || brew install ${p};
+    done
+fi

43e116a0bbcb476e655012f433e0dae31e5d14e9
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-11 21:31:45 +0100

    adds convinience methods for minishift

--- openshift.sh
+++ openshift.sh
@@ -9,6 +9,18 @@ if [ $? -eq 0 ]; then
         sed -e 's|__ltrim|#__ltrim|' -i '.bak' ~/.minishift-completion
     fi
     source ~/.minishift-completion
+
+    function minishift_init {
+        eval $(minishift docker-env)
+        eval $(minishift oc-env)
+        docker login -u developer -p `oc whoami -t` docker-registry-default.$(minishift ip).nip.io
+    }
+
+    function minishift_start {
+        minishift start --vm-driver virtualbox --memory "4GB" --cpus 4
+        minishift addons enable registry-route
+        minishift_init
+    }
 fi
 
 # openshift client

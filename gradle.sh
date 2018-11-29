de671915fd7497181d259b0b0849202194efa044
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-29 23:11:56 +0100

    adds bash completion for gradle

--- gradle.sh
+++ gradle.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+function __gradle_bash_completion {
+    if [[ ! -f $(brew --prefix)/etc/bash_completion ]]; then
+        echo "installing gradle bash completion"
+        brew install gradle-completion;
+    fi
+
+    . $(brew --prefix)/etc/bash_completion
+}
+
 # this wrapper function uses local gradle wrapper command if exists
 function gradle {
-	if [ -x 'gradlew' ]; then
+    __gradle_bash_completion
+
+	if [[ -x 'gradlew' ]]; then
 		./gradlew $@
 	else
 		bash -c "gradlew $*"

c8e9486af1bbcf88b4345ff5c6ecd3e3c9f646f9
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-10-06 22:10:44 +0200

    cleanup

--- tools/git.sh
+++ tools/git.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 set -o pipefail
-
 # https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
 
-if [ ${platform} = "darwin" ]; then
-    if [ ! -f /usr/local/etc/bash_completion ]; then
-        echo "installing first time git bash-completion"
-        brew install git bash-completion
-    fi
+if [ ! -f /usr/local/etc/bash_completion ]; then
+  if [ ! -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]; then
+    echo "installing first time git bash-completion"
+    brew install git bash-completion
+  fi
 fi
 
-if [ ${platform} = "darwin" ]; then
-    if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
-      . `brew --prefix`/etc/bash_completion.d/git-completion.bash
-    fi
+if [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]; then
+  . "$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
 fi
 
 if [ -f /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
   . /usr/local/etc/bash_completion.d/git-prompt.sh
-  export GIT_PS1_SHOWCOLORHINTS=true
-  export GIT_PS1_SHOWDIRTYSTATE=true
-  export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
-  export PS1='[\u@mbp \w$(__git_ps1)]\$ '
 fi
+
+export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_SHOWDIRTYSTATE=true
+export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
+export PS1='[\u@mbp \w$(__git_ps1)]\$ '

5b14d9039fa274ca75a8403e343c7de2ef56cb77
larsgohlke <lars.gohlke@deutschebahn.com>
2020-02-06 11:10:03 +0100

    adds alias for 'terraform state show' due to https://github.com/hashicorp/terraform/issues/21779

--- tools/terraform.sh
+++ tools/terraform.sh
@@ -2,4 +2,8 @@
 
 if command -v  terraform > /dev/null; then
   alias tf_apply='terraform apply -auto-approve'
+
+  # this fixes the output of ansi colors
+  # see https://github.com/hashicorp/terraform/issues/21779
+  alias tf_state_show='terraform state show -no-color'
 fi

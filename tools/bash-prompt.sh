cc03dd49b67d58fea46a582d69e048f3ba4aaef8
Lars K.W. Gohlke <lkwg82@gmx.de>
2020-05-03 19:41:50 +0200

    adds prompt for terraform workspace

--- tools/bash-prompt.sh
+++ tools/bash-prompt.sh
@@ -2,6 +2,13 @@
 
 set -o pipefail
 
+function terraform_prompt() {
+  if [ -d .terraform ]; then
+    workspace="$(command terraform workspace show 2>/dev/null)"
+    echo "${workspace}"
+  fi
+}
+
 if [[ ! -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
   echo "installing first time bash-git-prompt"
   brew install bash-git-prompt
@@ -15,5 +22,18 @@ if [[ -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
   GIT_PROMPT_ONLY_IN_REPO=1
   GIT_PROMPT_THEME=Solarized
   GIT_PROMPT_SHOW_UPSTREAM=1
+
+  function prompt_callback() {
+    # combine with terraform
+    if command -v terraform >/dev/null; then
+      echo " [\[\e[1m\]workspace\e[0m:$(terraform_prompt)]"
+    fi
+  }
+
   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+
+# terraform workspace
+if ! type -t git_prompt_config >/dev/null && command -v terraform >/dev/null; then
+  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\e[38;5;99m\]\[\e[1m\]\n\[\033[1;34m\]$(date +%H:%M)\[\033[0;0m\] [workspace:$(terraform_prompt)]\e[0m \$ '
 fi

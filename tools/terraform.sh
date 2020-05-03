182a44a3adde95fbf4ff41d4dea9b00ccfd63a70
Lars K.W. Gohlke <lkwg82@gmx.de>
2020-05-03 17:38:49 +0200

    cleanup: return early if command not available

--- tools/terraform.sh
+++ tools/terraform.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if command -v  terraform > /dev/null; then
-  alias tf_apply='terraform apply -auto-approve'
-
-  # this fixes the output of ansi colors
-  # see https://github.com/hashicorp/terraform/issues/21779
-  alias tf_state_show='terraform state show -no-color'
+if ! command -v  terraform > /dev/null; then
+  return
 fi
+
+alias tf_apply='terraform apply -auto-approve'
+
+# this fixes the output of ansi colors
+# see https://github.com/hashicorp/terraform/issues/21779
+alias tf_state_show='terraform state show -no-color'

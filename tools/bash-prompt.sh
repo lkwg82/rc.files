f58d9aea075fbbfd83e810b23072fedd4904a006
Lars K.W. Gohlke <lkwg82@gmx.de>
2020-05-03 17:47:32 +0200

    adds link to config

--- tools/bash-prompt.sh
+++ tools/bash-prompt.sh
@@ -2,7 +2,6 @@
 
 set -o pipefail
 
-
 if [[ ! -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
   echo "installing first time bash-git-prompt"
   brew install bash-git-prompt
@@ -10,6 +9,9 @@ fi
 
 if [[ -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
   __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+
+  # configs
+  # see https://github.com/magicmonty/bash-git-prompt#all-configs-for-bashrc
   GIT_PROMPT_ONLY_IN_REPO=1
   GIT_PROMPT_THEME=Solarized
   GIT_PROMPT_SHOW_UPSTREAM=1

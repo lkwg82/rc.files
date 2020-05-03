d255f7f284bbec7b2be00fa4fd76b348d3d4c054
Lars K.W. Gohlke <lkwg82@gmx.de>
2020-05-03 20:32:38 +0200

    adds prompt for terraform workspace /2

--- tools/bash-prompt.sh
+++ tools/bash-prompt.sh
@@ -2,13 +2,6 @@
 
 set -o pipefail
 
-function terraform_prompt() {
-  if [ -d .terraform ]; then
-    workspace="$(command terraform workspace show 2>/dev/null)"
-    echo "${workspace}"
-  fi
-}
-
 if [[ ! -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
   echo "installing first time bash-git-prompt"
   brew install bash-git-prompt
@@ -25,8 +18,8 @@ if [[ -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
 
   function prompt_callback() {
     # combine with terraform
-    if command -v terraform >/dev/null; then
-      echo " [\[\e[1m\]workspace\e[0m:$(terraform_prompt)]"
+    if command -v terraform >/dev/null && [ -d .terraform ]; then
+      echo " [\[\e[1m\]workspace\e[0m:$(terraform workspace show 2>/dev/null || echo "<error>")]"
     fi
   }
 
@@ -34,6 +27,32 @@ if [[ -f $(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh ]]; then
 fi
 
 # terraform workspace
-if ! type -t git_prompt_config >/dev/null && command -v terraform >/dev/null; then
-  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\e[38;5;99m\]\[\e[1m\]\n\[\033[1;34m\]$(date +%H:%M)\[\033[0;0m\] [workspace:$(terraform_prompt)]\e[0m \$ '
+if command -v terraform >/dev/null; then
+
+  function terraform_prompt() {
+    if [ -d .terraform ]; then
+      # consider git-prompt
+      if type -t git_prompt_config >/dev/null; then
+        repo=$(git rev-parse --show-toplevel 2>/dev/null)
+        if [[ -e $repo ]]; then
+          # inside of git repository and with git-prompt we do nothing
+          return
+        fi
+      fi
+
+      # save old prompt
+      if [[ -z $OLD_PS1 ]]; then
+        OLD_PS1=${PS1}
+      fi
+
+      PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\e[38;5;99m\]\[\e[1m\]\n\[\033[1;34m\]$(date +%H:%M)\[\033[0;0m\] [workspace:$(terraform workspace show 2>/dev/null || echo "<error>")]\e[0m \$ '
+    else
+      # recover old prompt
+      if [[ -n $OLD_PS1 ]]; then
+        PS1=${OLD_PS1}
+      fi
+    fi
+  }
+
+  PROMPT_COMMAND="terraform_prompt; $PROMPT_COMMAND"
 fi

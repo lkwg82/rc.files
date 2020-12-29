4d28cf5d893f1129c79c8108f72f005161abec0b
Lars K.W. Gohlke <lkwg82@gmx.de>
2020-12-29 19:56:38 +0100

    wraps installed gradle

--- tools/gradle.sh
+++ tools/gradle.sh
@@ -10,6 +10,8 @@ function __gradle_bash_completion() {
   . "$(brew --prefix)/etc/bash_completion"
 }
 
+_gradle_path=$(command -v gradle)
+
 # this wrapper function uses local gradle wrapper command if exists
 function gradle() {
   __gradle_bash_completion
@@ -17,6 +19,13 @@ function gradle() {
   if [[ -x 'gradlew' ]]; then
     ./gradlew "$@"
   else
-    bash -c "gradlew $*"
+    if command -v gradlew > /dev/null; then
+      gradlew "$@"
+    elif [[ -n $_gradle_path ]]; then
+      $_gradle_path "$@"
+    else
+      echo "missing gradle :/"
+      exit 1
+    fi
   fi
 }

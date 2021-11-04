79337e07d22941f3d7e5b69bfa9a32bbba81e0f9
Lars Gohlke <lars.gohlke@deutschebahn.com>
2021-11-04 11:16:17 +0100

    adds exa and bat as replacements for ls and cat

--- tools/bash.sh
+++ tools/bash.sh
@@ -10,11 +10,8 @@ shopt -s histappend                     # append to history, don't overwrite it
 # create temp dir and cd
 alias cdtmp='dir="$(mktemp -d)" && cd $dir'
 
-alias ll='ls -al'
-
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
-alias ls='ls -GFh'
 
 # create dir and cd
 function cdmkdir() {
@@ -28,3 +25,17 @@ export PATH=~/bin:$PATH
 # https://github.com/jbarlow83/OCRmyPDF
 # https://medium.com/@thucnc/convert-a-scanned-pdf-to-text-with-linux-command-line-using-ocrmypdf-1a2e8d50277f
 alias docker_ocrmypdf='docker run --rm  -i --user "$(id -u):$(id -g)" --workdir /data -v "$PWD:/data" jbarlow83/ocrmypdf'
+
+
+if ! command -v exa >/dev/null; then
+  brew install exa
+fi
+
+alias ls='exa -al'
+alias ll='exa -alhg'
+
+if ! command -v bat >/dev/null; then
+  brew install bat
+fi
+
+alias cat='bat'

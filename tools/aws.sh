9d3beb8dbe5005c44f85ee239d2ef06b101d53c1
Lars Gohlke <lars.gohlke@deutschebahn.com>
2021-12-17 14:04:45 +0100

    adds aws ssm examples

--- tools/aws.sh
+++ tools/aws.sh
@@ -3,3 +3,15 @@
 if command -v aws >/dev/null; then
   complete -C '/usr/local/bin/aws_completer' aws
 fi
+
+
+function aws_ssm_help(){
+  cat <<EOF
+  examples
+
+  aws ssm start-session --target i-0463a3cdb38933426 # start shell
+  aws ssm start-session --target i-0463a3cdb38933426 --document-name AWS-StartInteractiveCommand --parameter command='tail -n1000 -F /opt/control/log'
+
+  # more documents: https://eu-central-1.console.aws.amazon.com/systems-manager/documents?region=eu-central-1
+EOF
+}

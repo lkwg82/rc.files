ed20bc0353194e9eb718572278460ac1565a8068
Lars Gohlke <lars.gohlke@deutschebahn.com>
2022-01-12 17:30:19 +0100

    adds tf_init alias

--- tools/terraform.sh
+++ tools/terraform.sh
@@ -19,6 +19,7 @@ fi
 complete -C "$(command -v terraform)" terraform
 
 alias tf_apply='terraform apply -auto-approve'
+alias tf_init='terraform init'
 alias tf_output='terraform output'
 
 # this fixes the output of ansi colors

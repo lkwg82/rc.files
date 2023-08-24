50cf6914f7716fc4e1b4f1e86e17d6900c160f47
Lars Gohlke <lars.gohlke@deutschebahn.com>
2023-08-24 10:34:40 +0200

    adds k8 function to raw search resource on cluster

--- tools/kubernetes.sh
+++ tools/kubernetes.sh
@@ -8,3 +8,13 @@ function k8_eks_choose_cluster {
     | gum choose --height=20 \
     | xargs aws eks --region eu-central-1 update-kubeconfig --name
 }
+
+function k8_kubectl_find {
+
+    if [[ -z $1 ]]; then
+      echo "missing argument"
+    else
+      kubectl get -A crds --no-headers --ignore-not-found 2>/dev/null | awk '{ print $1 }' |xargs -I{} /bin/bash -c "echo '> searching in {}' && kubectl get -A {} 2>/dev/null | grep $1"
+      kubectl api-resources --verbs=list -o name 2>/dev/null | xargs -I{} -n 1 /bin/bash -c "echo '> searching in {}' && kubectl get {} --show-kind 2>/dev/null | grep $1"
+    fi
+}

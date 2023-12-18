31f8a792ffbc31dcd135e59483cd86c4687dc643
Lars Gohlke <lars.gohlke@deutschebahn.com>
2023-12-18 18:57:02 +0100

    workaround somewhat broken gum

--- tools/kubernetes.sh
+++ tools/kubernetes.sh
@@ -3,10 +3,16 @@
 __lazy_install gum
 
 function k8_eks_choose_cluster {
-  gum spin --show-output --spinner dot --title 'aws eks list is loading...' -- aws eks list-clusters --no-paginate --output text \
-    | awk '{ print $2 }' \
-    | gum choose --height=20 \
-    | xargs aws eks --region eu-central-1 update-kubeconfig --name
+  (
+    list_of_clusters=$(mktemp)
+    gum spin --show-output --spinner dot --title 'aws eks list is loading...' -- "$SHELL" -c "aws eks list-clusters --no-paginate --output text > $list_of_clusters"
+
+    awk '{ print $2 }' < "$list_of_clusters"\
+      | gum choose --height=20 \
+      | xargs aws eks --region eu-central-1 update-kubeconfig --name
+
+    rm "$list_of_clusters"
+  )
 }
 
 function k8_kubectl_find {

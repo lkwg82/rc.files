41decfa8a16413bb3571042e9c07cd05acc1f034
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-01-27 13:26:00 +0100

    adds docker functions

--- tools/docker.sh
+++ tools/docker.sh
@@ -11,3 +11,33 @@ function docker_build_run(){
     docker run --rm -ti $image $@
   fi
 }
+
+function __docker_containers() {
+  gum spin --show-output --spinner dot --title 'list containers ...' -- \
+    "$SHELL" -c "docker ps --format '{{.Names}} {{.ID}}' | sort "
+
+  local cid=""
+  cid=$( \
+    printf "%-30s %s\n"  \
+      $(gum spin --show-output --spinner dot --title 'list containers ...' -- \
+      "$SHELL" -c "docker ps --format '{{.Names}} {{.ID}}' | sort ") \
+    | gum choose \
+    | awk '{print $2}'
+  )
+  export CONTAINER_ID=$cid
+}
+
+function docker_inspect() {
+
+  ( # keep env clean
+    __docker_containers
+    docker inspect $* "$CONTAINER_ID" | jq
+  )
+}
+
+function docker_logs() {
+  ( # keep env clean
+    __docker_containers
+    docker logs --timestamps $* "$CONTAINER_ID"
+  )
+}

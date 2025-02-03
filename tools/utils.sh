1583ec90a3236ffd0775e6fd65ac8e377dc32f64
Lars K.W. Gohlke <lkwg82@gmx.de>
2025-02-03 18:39:06 +0100

    adds function to interactive install/update ghostty on ubuntu

--- tools/utils.sh
+++ tools/utils.sh
@@ -12,3 +12,18 @@ function to_clipboard {
     cat
   fi
 }
+
+function install_or_update_ghostty {
+  # see https://github.com/mkasberg/ghostty-ubuntu
+
+  source /etc/os-release
+  local ARCH=$(dpkg --print-architecture)
+  local GHOSTTY_DEB_URL=$(
+     curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest | \
+     grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_${ARCH}_${VERSION_ID}.deb"
+  )
+  local GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
+  curl -LO "$GHOSTTY_DEB_URL"
+  sudo dpkg -i "$GHOSTTY_DEB_FILE"
+  rm -v "$GHOSTTY_DEB_FILE"
+}

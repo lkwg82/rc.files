786913d5b94f3ef9d4cbab2fabb87744448df842
Lars Gohlke <lars.gohlke@deutschebahn.com>
2026-04-04 09:33:44 +0200

    adds local state to allowed write directories

--- tools/ai.sh
+++ tools/ai.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # see https://nono.sh/
-alias nono_opencode_ide='nono run --profile opencode --allow-cwd --read $HOME/.copilot --read $HOME/ghorg -- opencode --hostname 127.0.0.1 --port 4096 --continue'
+alias nono_opencode_ide='nono run --profile opencode --allow-cwd --read $HOME/.copilot --read $HOME/ghorg --allow $HOME/.local/state/opencode -- opencode --hostname 127.0.0.1 --port 4096 --continue'
